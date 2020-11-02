93b83a3fa974281f4a9a6f9b5838fc999345d300
Kirill <m-group-react@tutanota.com>
2020-11-02 11:05:29 +0200

    New Post Form saves to the backend

--- src/components/blogpost/blogpost-form.jsx
+++ src/components/blogpost/blogpost-form.jsx
@@ -1,8 +1,56 @@
+import { create } from '../../services/blogpost.js'
 const { Component } = require("react")
 
 class PostForm extends Component {
+    static defaultProps = {
+        mode: 'new'
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            text: '',
+            tags: []
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+    // componentDidMount() {
+
+    // }
+
+    handleSubmit(event) {
+        const toSave = {
+            text: this.state.text,
+            title: this.state.title,
+            userID: 'user 01',
+            lastUpdateAt: Date.now()
+        };
+        create(toSave);
+        event.preventDefault();
+    }
+
+    handleChange(event) {
+        this.setState({ [event.target.id]: event.target.value });
+    }
+
     render() {
-        return (<div>Blog Post</div>);
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>Title
+                <input id='title' value={this.state.title} onChange={this.handleChange} type='text'></input>
+                </label>
+                <label>
+                    Text:
+              <textarea id='text' value={this.state.text} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
     }
 }
 

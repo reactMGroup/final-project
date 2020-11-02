59f65a5d2d83726ccac785884058ffc4a55ea3d4
Kirill <m-group-react@tutanota.com>
2020-11-02 10:20:04 +0200

    Router added.

---
+++ src/components/blogpost/blogpost-form.jsx
@@ -0,0 +1,9 @@
+const { Component } = require("react")
+
+class PostForm extends Component {
+    render() {
+        return (<div>Blog Post</div>);
+    }
+}
+
+export default PostForm;
--- src/components/main-window.jsx
+++ src/components/main-window.jsx
@@ -1,13 +1,23 @@
 import { Component } from "react";
 import PostsList from "./blogpost/posts-list";
 import MainMenu from "./main-menu";
+import { Router, Route, Redirect, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import PostForm from "./blogpost/blogpost-form";
 
 class MainWindow extends Component {
     render() {
-        return (<div>
-            <MainMenu />
-            <PostsList />
-        </div>)
+        return (
+            <Router history={createBrowserHistory()}>
+                <MainMenu />
+                <Switch>
+                    <Route exact path="/feed"><PostsList /></Route>
+                    <Route exact path="/new"><PostForm /></Route>
+                    <Route exact path="/about" render={() => (<div>About</div>)}></Route>
+                    <Redirect from="/" to="/feed" />
+                </Switch>
+            </Router>
+        )
     }
 }
 

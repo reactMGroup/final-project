0ff0393aa1013c43ec82c1d67ec63bd0e4bac3e1
Kirill <m-group-react@tutanota.com>
2020-11-04 16:00:46 +0200

    Feed sorted by last update desc

--- src/services/blogpost.js
+++ src/services/blogpost.js
@@ -4,18 +4,32 @@ import { getUser } from './users';
 
 const endPoint = '/blogpost/';
 
+function byUdatedDesc(post1, post2) {
+    if (post1.lastUpdateAt < post2.lastUpdateAt) {
+        return 1;
+    }
+    if (post1.lastUpdateAt > post2.lastUpdateAt) {
+        return -1;
+    }
+    return 0;
+}
+
 export default function getBlogpostAll() {
     const url = rootUrl + endPoint;
     // const posts = axios.get(url).then();
     const result = axios.get(url)
-        .then((result) => result.data.map(blogpost => {
-            return getUser(blogpost.userID).then(userResult => {
-                const user = userResult.data;
-                blogpost.userFullname = `${user.name.title} ${user.name.first} ${user.name.last}`;
-                blogpost.userPicture = user.picture.thumbnail;
-                return blogpost;
-            });
-        }));
+        .then((result) => {
+            const allPosts = result.data;
+            allPosts.sort(byUdatedDesc);
+            return allPosts.map(blogpost => {
+                return getUser(blogpost.userID).then(userResult => {
+                    const user = userResult.data;
+                    blogpost.userFullname = `${user.name.title} ${user.name.first} ${user.name.last}`;
+                    blogpost.userPicture = user.picture.thumbnail;
+                    return blogpost;
+                });
+            })
+        });
     return result.then(array => Promise.all(array));
 }
 
